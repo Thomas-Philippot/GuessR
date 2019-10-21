ce42ed62032662548887c15f76e453d0bfebce16
PHILIPPOT Thomas <thomas.philippot2018@campus-eni.fr>
2019-10-21 11:37:13 +0200

    no status bar

--- app/src/main/java/fr/eni/guessr/MainActivity.java
+++ app/src/main/java/fr/eni/guessr/MainActivity.java
@@ -7,8 +7,11 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.OvershootInterpolator;
 
 import com.github.clans.fab.FloatingActionMenu;
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         this.actionMenu = this.findViewById(R.id.menu);
 
         this.createCustomAnimation();
