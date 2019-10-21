638de486cb9f704e80a4c2cfc988b368c780ad13
PHILIPPOT Thomas <thomas.philippot2018@campus-eni.fr>
2019-10-21 11:00:10 +0200

    navigation with floating action menu

--- app/src/main/java/fr/eni/guessr/MainActivity.java
+++ app/src/main/java/fr/eni/guessr/MainActivity.java
@@ -2,13 +2,38 @@ package fr.eni.guessr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionMenu actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.actionMenu = this.findViewById(R.id.menu);
+    }
+
+    public void resultClicked(View view) {
+        Intent itent = new Intent(this, ResultsActivity.class);
+        this.actionMenu.close(true);
+        this.startActivity(itent);
+    }
+
+    public void proposalClicked(View view) {
+        Intent itent = new Intent(this, ProposalActivity.class);
+        this.actionMenu.close(true);
+        this.startActivity(itent);
+    }
+
+    public void aboutClicked(View view) {
+        Intent itent = new Intent(this, AboutActivity.class);
+        this.actionMenu.close(true);
+        this.startActivity(itent);
     }
 }
